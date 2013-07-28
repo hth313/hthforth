66c68d92065fb62becb137b21027565f649ac520
Håkan Thörngren <hth313@gmail.com>
2013-07-28 16:36:45 +0200

    Catch and show error exceptions at top level and stay in Forth environment.

--- src/Forth/Forth.hs
+++ src/Forth/Forth.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-
   This file is part of CalcForth.
   Copyright Håkan Thörngren 2011
@@ -8,15 +9,29 @@
 
 module Main (main) where
 
+import Control.Monad.Trans
 import Data.Int
 import Forth.Cell
 import Forth.Core
 import Forth.Machine
 import Forth.Target
 import Util.Endian
+import Control.Monad.CatchIO
+import Control.Exception (IOException, AsyncException)
+import System.Exit
+import System.IO
+
 
 main =
     let target = Target 4 4 1 LittleEndian :: Target Int32
     in do
       putStrLn "The Planet Pluto Forth, version 1.1.1"
-      evalStateT (addNatives >> quit) (initialState target)
+      evalStateT (addNatives >> topLevel) (initialState target)
+
+topLevel =
+    let showExc e = liftIO $ hPutStrLn stderr (show e)
+    in do
+      quit `catches` [ Handler (\(e :: IOException) -> showExc e),
+                       Handler (\(e :: AsyncException) -> showExc e),
+                       Handler (\(e :: ForthException) -> showExc e) ]
+      topLevel
