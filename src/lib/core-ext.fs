ca1726c0e988ad33ec94167d5a2f5fd5e42e9555
Håkan Thörngren <hth313@gmail.com>
2013-08-07 23:13:38 +0200

    Updated core-ext.fs with \, NIP and TUCK.

--- src/lib/core-ext.fs
+++ src/lib/core-ext.fs
@@ -3,6 +3,19 @@
 0    CONSTANT FALSE
 0 0= CONSTANT TRUE
 
+: \
+    1 PARSE-DO
+      I @ 10 = IF LEAVE THEN 1+
+    LOOP
+    >IN +! ; IMMEDIATE
+
+
+: NIP  ( x1 x2 -- x2 )
+    SWAP DROP ;
+
+: TUCK  ( x1 x2 -- x2 x1 x2 )
+    SWAP OVER ;
+
 \ PARSE reside among the core words in this implementation as it is used
 \ as a building block for WORD.
 
