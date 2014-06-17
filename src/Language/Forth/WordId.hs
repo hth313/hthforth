6c429f01837bc22bfb4ff856ad430b718b5f52cc
Håkan Thörngren <hth313@gmail.com>
2014-06-17 16:39:51 +0200

    As WordId is now a proper newtype, added unWordId as a means to convert one to Int.

--- src/Language/Forth/WordId.hs
+++ src/Language/Forth/WordId.hs
@@ -1,6 +1,6 @@
 module Language.Forth.WordId (WordId(..)) where
 
 -- | Unique identifier for words.
-newtype WordId = WordId Int deriving (Eq, Ord)
+newtype WordId = WordId { unWordId :: Int } deriving (Eq, Ord)
 instance Show WordId where
   show (WordId n) = show n
