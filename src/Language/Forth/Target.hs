aab2ab3800f3a4c264b36d234edb17f2235e7953
Håkan Thörngren <hth313@gmail.com>
2014-07-12 22:35:55 +0800

    Reformatted the Target structure.

--- src/Language/Forth/Target.hs
+++ src/Language/Forth/Target.hs
@@ -6,9 +6,9 @@ import Util.Memory
 
 
 -- | Target descriptor
-data Target cell =  Target {
-    bytesPerCell :: Int,                     -- ^ native value size
-    bytesPerInstruction :: Int,              -- ^ execution token size
-    bytesPerChar :: Int,                     -- ^ character size
-    endian :: Endian
-    }
+data Target cell =  Target
+  { bytesPerCell :: Int                     -- ^ native value size
+  , bytesPerInstruction :: Int              -- ^ execution token size
+  , bytesPerChar :: Int                     -- ^ character size
+  , endian :: Endian
+  }
